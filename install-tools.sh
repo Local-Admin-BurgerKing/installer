0e356d619f4b053306ff9454aa3cb863b19b4f92
Nils Brugger <nbrugger@student.tgm.ac.at>
2019-11-07 19:21:37 +0100

    Add Validation Tools
    and python

--- install-tools.sh
+++ install-tools.sh
@@ -1,4 +1,8 @@
 apt update
 apt upgrade
-apt install git mvn npm
+apt install git mvn npm python
 npm install redoc-cli -g
+npm install openapi-enforcer -g
+npm install speccy -g
+pip install openapi-spec-validator
+npm install -g swagger-cli
