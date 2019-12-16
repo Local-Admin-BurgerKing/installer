5d97b70bc9a947934fac6cc8f386452e760ea274
nbrugger-tgm <nbrugger@sudent.tgm.ac.at>
2019-12-16 20:41:37 +0100

    Fix Serviceworker Buildscript

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,20 +2,22 @@ rm exportet -r
 mkdir exportet
 
 cd core
-mvn install
+mvn clean install
 cd ..
+
+
 cd rest-api
 mvn clean package
 cp target/*.war ../exportet/rest-api-war.jar
 cd ..
 
 cd webpage
-mvn package
+mvn clean package
 cp target/*.war ../exportet/webpage.war
 cp target/*.war ../exportet/ROOT.war
 cd ..
 
 cd service-worker
-mvn package
-cp target/*-dependencies.jar ../exportet/ws.jar
+mvn clean  package -P "Jar With Dependencies"
+cp target/*-dependencies.jar ../exportet/sw.jar
 cd ..
